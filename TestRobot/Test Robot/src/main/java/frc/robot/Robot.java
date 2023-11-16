b2ca4fd2c3500483d16cfa2181ed7fadace05561
Frank Herring <fhh0002@tigermail.auburn.edu>
2023-11-15 20:22:04 -0600

    initial commit

---
+++ TestRobot/Test Robot/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,159 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  private final PWMSparkMax m_onlyDrive = new PWMSparkMax(0);
+  private final XboxController m_controller = new XboxController(0);
+  private final Timer m_timer = new Timer();
+  private final NetworkTableEntry ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty");
+
+  private RobotContainer m_robotContainer;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    m_timer.restart();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    // private NetworkTableEntry ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty");
+
+    double h_cam = 14.5;
+    double h_target = 25.0;
+    double angle_of_camera = 0.0; // in degrees
+    double angle_of_target = ty.getDouble(0.0);
+
+    double distance_from_target = (h_target - h_cam) / Math.tan(Math.toRadians(angle_of_camera + angle_of_target));
+    
+    SmartDashboard.putNumber("Height of Camera:", h_cam);
+    SmartDashboard.putNumber("Height of Target:", h_target);
+    SmartDashboard.putNumber("Angle of Camera from Level Plane:", angle_of_camera);
+    SmartDashboard.putNumber("Angle of April Tag:", angle_of_target);
+    SmartDashboard.putNumber("Distance:", distance_from_target);
+
+    if (distance_from_target > 40.0 && distance_from_target < 999999) {
+      m_onlyDrive.set(0.5);
+    } else {
+      m_onlyDrive.stopMotor();
+    }
+
+    // Drive for 2 seconds
+    // if (m_timer.get() < 2.0) {
+    //   // Drive forward at half-speed
+    //   m_onlyDrive.set(0.5);
+    // } else {
+    //   m_onlyDrive.stopMotor();
+    // }
+  }
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    double controllerRightYPosition = m_controller.getRightY();
+    SmartDashboard.putNumber("Xbox Right Y Value:", controllerRightYPosition);
+
+    if (controllerRightYPosition <= 0.05 && controllerRightYPosition >= -0.05) {
+      m_onlyDrive.stopMotor();
+    } else {
+      m_onlyDrive.set(controllerRightYPosition);
+    }
+  }
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
